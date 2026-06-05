6d53eec63f1eac5ddfaccddf8c24b6ca418032ec
Tappaev Dias <limbriko@mail.ru>
2026-06-05 19:49:20 +0500

    исправил + добавил css

--- src/components/Anime.tsx
+++ src/components/Anime.tsx
@@ -1,25 +1,27 @@
 import { useEffect, useState } from "react"
 
 export default function AnimeTierList() {
-    const controller = new AbortController()
+    
     const [Anime, setAnime] = useState<any[]>([])
     const [Load, isLoad] = useState(true)
     useEffect(() => {
+        const controller = new AbortController()
         fetch('https://api.jikan.moe/v4/top/anime?limit=6', {signal: controller.signal})
             .then(response => response.json())
             .then(json => setAnime(json.data))
             .then(() => isLoad(false))
-            .catch(error => console.error('Ошибка '+ error))} 
-        ,[controller.signal])
+            .catch(error => console.error('Ошибка '+ error))
+        return () => controller.abort()} 
+        ,[])
     return(
-        <div className="flex flex-col gap-2 p-8 sm:flex-row sm:items-center sm:gap-6 sm:py-4 bg-green-" >
-            <div className="">
+        <div className="flex flex-col gap-2 p-8 sm:flex-row sm:items-center sm:gap-6 sm:py-4 min-h-screen bg-grey-800" >
+            <div className="grid grid-cols-3 gap-4">
                 {Load ? <p>Загрузка...</p>: Anime.map((anime) => (
                 <div key = {anime.mal_id}> 
-                <div className="">
+                <div className="flex flex-col items-center justify-center  bg-gray-950 text-white">
                     <img src={anime.images.jpg.image_url}/>
                 </div>
-                <p className="dark:text-white">{anime.title}</p>
+                <p className="flex flex-col items-center justify-center  bg-gray-950 text-white">{anime.title}</p>
                 </div> ))}
         </div>
         </div>
--- src/components/TopCharacters.tsx
+++ src/components/TopCharacters.tsx
@@ -1,23 +1,27 @@
 import { useEffect, useState } from "react";
 
 export default function Character(){
-    const controller = new AbortController()
+    
     const [Character, setCharacter] = useState([])
     const [Loading, isLoading] = useState(true)
-    useEffect(()=>{
+    useEffect(() => {
+        const controller = new AbortController()
         fetch('https://api.jikan.moe/v4/top/characters?limit=6', {signal: controller.signal})
         .then(element => element.json())
         .then(char => setCharacter(char.data))
         .then(() => isLoading(false))
-    }, [controller.signal])
+        return () => controller.abort()
+    }, [])
     return(
-        <div>
-            {Loading ? <p> Loading...</p>: Character.map((step) =>
+        <div className="flex flex-col gap-2 p-8 sm:flex-row sm:items-center sm:gap-6 sm:py-4 min-h-screen bg-grey-800">
+            {Loading ? <p> Loading...</p>: Character.map((step) => (
             <div key={step.mal_id}> 
-            <img className = 'h-32 w-32 align-content' src = {step.images.jpg.image_url}/>
-            <p>{step.name}</p>
+            <div className="flex flex-col items-center justify-center  bg-gray-950 text-white"> 
+                <img src = {step.images.jpg.image_url}/>
             </div>
-             )}
+            <p className="flex flex-col items-center justify-center bg-gray-950 text-white">{step.name}</p>
+            </div>
+             ))}
         </div>
     )
 }
